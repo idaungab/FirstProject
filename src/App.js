2875eac06b924fa229f50b53643f3dd19c1f8ca9
Loida Mae <idaungab@icloud.com>
2017-11-03 13:38:23 +0800

    Edit PORT to 1114 from 2017 Second Table deleted

--- src/App.js
+++ src/App.js
@@ -144,33 +144,7 @@ removeCountry(id){
                 />
         </Table>
 
-        <Table
-            rowHeight={50}
-            rowsCount={rows.length}
-            width={500}
-            height={500}
-            headerHeight={50}>
-            <Column
-              header={<Cell>Col 1</Cell>}
-              cell={<Cell>Column 1 static content</Cell>}
-              width={200}
-            />
-            <Column
-              header={<Cell>Col 2</Cell>}
-              cell={<Cell></Cell>}
-              width={100}
-            />
-            <Column
-              header={<Cell>Col 3</Cell>}
-              cell={({rowIndex, ...props}) => (
-                <Cell {...props}>
-                  Data for column 3: {rows[rowIndex][2]}
-                </Cell>
-              )}
-              width={200}
-            />
-         </Table>
-
+      
 
       </div>
     );
